e6f640c911da8862164b805da3b90fb49af77cb3
Anders Forsman <andeye@cyancore.net>
2017-05-10 16:54:51 +0300

    Added more gears to puzzle and testing some random positions

--- src/App.ts
+++ src/App.ts
@@ -37,7 +37,9 @@ export class App {
 			actors: [
 				{
 					actor: this.player,
-					layer: 'walkway'
+					layer: 'walkway',
+					x: 0,
+					y: 0
 				}
 			]
 		});
@@ -55,9 +57,64 @@ export class App {
 						id: 'gear_small',
 						sheetUrl: 'assets/gear_small.png',
 						firstFrame: 0
-					}
-					),
-					layer: 'puzzle'
+					}),
+					layer: 'puzzle',
+					x: 100,
+					y: 100
+				}, {
+					actor: new Actor({
+						id: 'gear_big',
+						sheetUrl: 'assets/gear_big.png',
+						firstFrame: 0
+					}),
+					layer: 'puzzle',
+					x: 300,
+					y: 150
+				}, {
+					actor: new Actor({
+						id: 'gear_big_half',
+						sheetUrl: 'assets/gear_big_half.png',
+						firstFrame: 0
+					}),
+					layer: 'puzzle',
+					x: 300,
+					y: 400
+				}, {
+					actor: new Actor({
+						id: 'gear_ratchet',
+						sheetUrl: 'assets/gear_ratchet.png',
+						firstFrame: 0
+					}),
+					layer: 'puzzle',
+					x: 500,
+					y: 500
+				}, {
+					actor: new Actor({
+						id: 'pawl',
+						sheetUrl: 'assets/pawl.png',
+						firstFrame: 0
+					}),
+					layer: 'puzzle',
+					x: 500,
+					y: 500
+				}, {
+					actor: new Actor({
+						id: 'gear_stepper_a',
+						sheetUrl: 'assets/gear_stepper_a.png',
+						firstFrame: 0
+					}),
+					layer: 'puzzle',
+					x: 100,
+					y: 400
+				}, {
+					actor: new Actor({
+						id: 'gear_stepper_b',
+						sheetUrl: 'assets/gear_stepper_b.png',
+						firstFrame: 0
+					}),
+					layer: 'puzzle',
+					x: 150,
+					y: 400
 				}
 			]
 		});
